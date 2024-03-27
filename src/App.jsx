cfb30066d59c44c01a3fc00b844290d8da6b7823
unknown <shambelmighty51@gmail.com>
2024-03-27 22:49:55 +0100

    blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-
-import NavBar from "./components/NavBar.jsx"
-import Desc from "./components/Desc.jsx"
+import NavBar from "./components/NavBar.jsx";
+import Desc from "./components/Desc.jsx";
+import Cards from "./components/Cards.jsx";
+import data from "./data.js";
+import Footer from "./components/Footer.jsx";
 function App() {
-
+  const Card = data.map((item) => {
+    return <Cards key={item.id} item={item} />;
+  });
   return (
     <>
+      <NavBar />
+      <Desc />
+
       <div>
-        <NavBar />
-        <Desc/>
-        
+        <div className="pb-20">  
+          <span className="text-2xl px-10">Movies</span>
+          <div className="grid grid-cols-5 ">{Card}</div>
+        </div>
+
+        <span className="text-2xl px-10 py-10"> TV Shows</span>
+        <div className="grid grid-cols-5">{Card}</div>
       </div>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,28 @@
-function Cards() {
-    return {
-        
+export default function Card(props) {
+    let badgeText
+  
+    if (props.item.openSpots === 0) {
+      badgeText="SOLD OUT"
+    }else if (props.item.location === "Online"){
+     badgeText="ONLINE"
     }
-}
+    return (
+      <div className="flex-shrink-0 ml-10 mr-0 relative ">
+        
+        {badgeText && <div className="absolute top-6 m-1 px-2 bg-white rounded-sm font-bold">{badgeText}</div>}
+        <img src={`../images/${props.item.coverImg}`} className="py-2  rounded-lg w-11/12 border-radius: 9px; " alt="katie swimming" />
+        
+        <div className="flex text-sm ">
+          <img className="rounded-md w-4 h-4" src="star.png" alt="icon" />
+          <span className="text-gray-400">{props.item.stats.rating}</span>
+          <span className="text-gray-400">({props.item.stats.reviewCount})• </span>
+          <span className="text-gray-400">{props.item.location}</span>
+        </div>
+        <div className="text-sm py-2  ">
+          <h2>{props.item.title}</h2>
+            <span className="font-bold">From ${props.item.price}/person</span>
+  
+        </div>
+      </div>
+    );
+  }
