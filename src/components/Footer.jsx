369c138facbf2226a85a7355dbde699d8d37cbb0
unknown <shambelmighty51@gmail.com>
2024-05-08 23:23:20 +0200

    n

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,32 @@
 // eslint-disable-next-line react/prop-types
 function Footer({darkMode}) {
-    return <div className={darkMode ? "dark bg-gray-900  p-4 mx-auto text-white" : "flex flex-wrap items-center justify-between mx-auto p-4 bg-blue-900"}>
-    <a href="https://myflixerz.to/home" className="flex items-center space-x-3 rtl:space-x-reverse ">
+    return <div className={darkMode ? "dark bg-gray-900 flex flex-wrap items-center justify-between p-4 mx-auto text-white" : "flex flex-wrap items-center justify-between mx-auto p-4 bg-blue-900"}>
+      <a href="https://myflixerz.to/home" className="flex items-center space-x-3 rtl:space-x-reverse ">
         <img src="myfixer.jpg" className="h-8" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">My Flixer</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-200 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      
-    </div>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Popcorn</span>
+        </a>
+         
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-900  md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a href="#" className="block py-2 px-3 text-white  rounded md:bg-transparent md:text-white md:p-0 dark:text-white md:dark:text-white md:hover:text-blue-100" aria-current="page">Home</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-100 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Genere</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-100 md:p-0 dark:text-white md:dark:hover:text-blue-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Country</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-100 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Movies</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-100 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">TV Shows</a>
+                    </li>
+                </ul>
+            </div>
+   
+   
       </div>
 }
 export default Footer;
